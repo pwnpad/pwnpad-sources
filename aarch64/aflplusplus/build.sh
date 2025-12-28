1b26fb976c2f1eae35feebc6b50e0498b5e59432
Platy <daryl.limkz@gmail.com>
2025-12-28 09:19:27 +0800

    build: updated sources for aflplusplus

--- aarch64/aflplusplus/build.sh
+++ aarch64/aflplusplus/build.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 
-VERSION="4.34c"
+VERSION="4.35c"
 
 # Ubuntu build script
-# docker run --rm -it ubuntu:25.04
+# docker run --rm -it ubuntu:26.04
 apt-get update
-apt-get install -y build-essential python3-dev automake cmake git flex bison libglib2.0-dev libpixman-1-dev python3-setuptools cargo libgtk-3-dev
-apt-get install -y lld-14 llvm-14 llvm-14-dev clang-14 || apt-get install -y lld llvm llvm-dev clang
+DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential python3-dev automake cmake git flex bison libglib2.0-dev libpixman-1-dev python3-setuptools cargo libgtk-3-dev
+# try to install llvm-18 and install the distro default if that fails
+apt-get install -y lld-18 llvm-18 llvm-18-dev clang-18 || apt-get install -y lld llvm llvm-dev clang
 apt-get install -y gcc-$(gcc --version|head -n1|sed 's/\..*//'|sed 's/.* //')-plugin-dev libstdc++-$(gcc --version|head -n1|sed 's/\..*//'|sed 's/.* //')-dev
-apt-get install -y python3-pip
-apt-get install -y ninja-build
+apt-get install -y meson ninja-build # for QEMU mode
+apt-get install -y cpio libcapstone-dev # for Nyx mode
+apt-get install -y wget curl # for Frida mode
+apt-get install -y python3-pip python3-venv # for Unicorn mode
+# for qemu mode
 apt-get install -y gcc-aarch64-linux-gnu
 apt-get install -y gcc-i686-linux-gnu
 apt-get install -y gcc-x86-64-linux-gnu
 
 cd $HOME
 
-git clone --branch=v${VERSION} https://github.com/AFLplusplus/AFLplusplus
+git clone --branch=v${VERSION} --depth=1 https://github.com/AFLplusplus/AFLplusplus
 cd AFLplusplus
+git submodule update --init
 
 MAKEFLAGS="-j$(nproc)" make distrib
 
@@ -44,4 +49,4 @@ ln -s afl-qemu-trace-$(uname -m) $HOME/usr/bin/afl-qemu-trace
 
 cd $HOME
 
-tar czvf aflplusplus-${VERSION}.tgz usr/
+tar czvf aflplusplus-${VERSION}-$(uname -m).tgz usr/
